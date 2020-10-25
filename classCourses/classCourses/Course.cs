73153d714c57a5c39a83332cdcf3b940a172bef9
Victor CAZAUX <23166@student.dorset-college.ie>
2020-10-25 12:27:33 +0100

    class course v0

---
+++ classCourses/classCourses/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Course         //Victor CAZAUX , Théo UNDERWOOD , Nicolas GONCALVES , BENJAMIN TOUBIANA , FOUCAUD BONNEFONT , Thomas CULINO
+    {
+        string name;
+        double duration;
+        string coursePlan;
+        // FacultyMember prof; //need to create FacultyMember class
+        string room;
+
+        public Course(string name, double duration, string courseplan, FacultyMember prof, string room)
+        {
+            this.name = name;
+            this.duration = duration;
+            // this.coursePlan = coursePlan;
+            this.prof = prof;
+            this.room = room;
+        }
+
+
+        public string toStringCoursePlan()
+        {
+            return "The course name is : " + name + ", the course duration is : " + duration + " minutes, the professor is :" + prof.Name + " and the course will be in room : " + room;
+        }
+
+
+    }
+}
