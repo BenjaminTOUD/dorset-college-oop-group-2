ce99e43057008e0e7118358252064774d718fe06
Benjamin Toubiana <23206@student.dorset-college.ie>
2020-10-27 17:54:53 +0100

    User class created

---
+++ ConsoleApp6/ConsoleApp6/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Project
+{
+    public abstract class User
+    {
+        int ID;
+        string name;
+        private string password;
+        private string adress;
+        private string inscriptionDate;
+        private string phoneNumber;
+        
+        public void Login()
+        {}
+        public void ChangePassword()
+        {}
+        public void ChangeAdress()
+        {}
+
+        public override string ToString() //Ne pas afficher le password
+        {
+            return password;
+        }
+    }
+}
