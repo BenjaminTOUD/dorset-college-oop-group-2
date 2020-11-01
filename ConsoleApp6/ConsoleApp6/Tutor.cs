55cfa9fd4b00c2bcb13346007ce4f41807a5a337
tioutbonnefont <71397371+tioutbonnefont@users.noreply.github.com>
2020-11-01 15:54:30 +0100

    Update Tutor.cs

--- ConsoleApp6/ConsoleApp6/Tutor.cs
+++ ConsoleApp6/ConsoleApp6/Tutor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,44 @@ namespace Project
     // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
     class Tutor : FacultyMember
     {
+        public List<Course> courseList;
         public Tutor(List<Course> courseList) : base(courseList)
         {
-
+            this.courseList = courseList;
         }
-
+        public int[] IsStudent(Student student)
+        {
+            int[] result = new int[2];
+            result[0] = -1;
+            result[1] = -1;
+            for(int i=0;i<this.courseList.Count;i++)
+            {
+                for(int j=0;j<courseList[i].clas.Count;j++)
+                {
+                    if (courseList[i].clas[j]==student)
+                    {
+                        result[0]=i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            
+            }
+            return result;
+        }
         public void StudentDetail(Student student)
         {
-            Console.WriteLine("ID : " + User.ID + ", Name: " + User.Name + ", Adress : " + User.Adress + ", Phone : " + User.PhoneNumber);
+            if (IsStudent(student)[0] == -1)
+            {
+                Console.WriteLine("This student is not in any of your courses");
+            }
+            int i = IsStudent(student)[0];
+            int j = IsStudent(student)[1]; 
+            else
+            {
+                Console.WriteLine("ID : " + courseList[i].clas[j].ID + ", Name: " + courseList[i].clas[j].Name + ", Adress : " + courseList[i].clas[j].Adress + ", Phone : " + courseList[i].clas[j].PhoneNumber);
+            }
+        
         }
     }
 }
