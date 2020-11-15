4ffc4bf74376a20187fb9f1b3edceb4288ff08c3
tculino <71397368+tculino@users.noreply.github.com>
2020-11-15 14:58:00 +0100

    Admin comments
    Added comments and finished the CreateTT function

--- ConsoleApp6/ConsoleApp6/Admin.cs
+++ ConsoleApp6/ConsoleApp6/Admin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,74 +9,97 @@ namespace Project
     class Admin : User
     {
         // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
-        public Admin()
+        public Admin(int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber) : base(ID, name, password, adress, inscriptionDate, phoneNumber)
         {
-            #region test
-            List<Course> a = new List<Course>();
-            FacultyMember b = new FacultyMember(a);
-            Course az = new Course("maths", 1.5, "blabla", b, "L309");
-            Course aze = new Course("english", 1.5, "blabla", b, "L309");
-            Course azer = new Course("french", 1.5, "blabla", b, "L309");
-            Course azert = new Course("physics", 1.5, "blabla", b, "L309");
-            a.Add(az);
-            a.Add(aze);
-            a.Add(azer);
-            a.Add(azert);
-            CreateTT(a);
-            //CreateCourse("maths", 1.5, "blabla", b, "L309");
-            #endregion
-        }
 
-        public void CreateCourse(string name, double duration, string coursePlan, FacultyMember prof, string room)
-        {
-            new Course(name, duration, coursePlan, prof, room);
-            Console.WriteLine($"the course {name} has been created");
         }
-        public void CreateTT(List<Course> course)   //Work In Progress
+        //creating a course 
+        public void CreateCourse(string name, double duration, string coursePlan, FacultyMember prof, string room, List<Student> clas)
         {
-            List<List<string>> newTT = new List<List<string>>();
-            for (int i = 0; i < 5; i++)
+            new Course(name, duration, coursePlan, prof, room, clas);
+            Console.WriteLine($"the course {name} has been created");   //confirmation that the course has been created
+        }
+        //Creating and displaying a timetable
+        public void CreateTT(List<Course> course)
+        {
+            string[,] newTT = new string[6, 6];
+            //filling the first line
+            newTT[0, 0] = "     ";
+            newTT[0, 1] = "Monday";
+            newTT[0, 2] = "Tuesday";
+            newTT[0, 3] = "Wednesday";
+            newTT[0, 4] = "Thursday";
+            newTT[0, 5] = "Friday";
+            //filling the first column
+            for (int i = 1; i < 6; i++)
             {
-                List<string> alist = new List<string>();
-                newTT.Add(alist);
-                newTT[i].Add($"day {i + 1} ");
+                newTT[i, 0] = $"{8 + 2 * (i - 1)}h";
+                //Adding spaces to have a beautiful timetable on the console
+                while (newTT[i,0].Length != 5)
+                {
+                    newTT[i, 0] += " ";
+                }
+                while (newTT[0,i].Length != 10)
+                {
+                    newTT[0, i] += " ";
+                }
             }
-            Random generate = new Random();
+            Random generator = new Random();
             int day = -1;
+            int hour = -1;
+            //Placing the courses using random generators
             foreach (Course aCourse in course)
             {
-                do
+                day = generator.Next(1, 6);  //generating the day
+                int count = 0;
+                for (int i = 1; i < 6; i++)
                 {
-                    day = generate.Next(5);
-                    if (newTT[day].Count() < 3)
+                    if (newTT[i, day] != null) count++; //counting the number of courses a day to have at most 3 classes everyday 
+                }
+                if (count < 3)
+                {
+                    do
                     {
-                        newTT[day].Add(aCourse.Name);
+                        hour = generator.Next(1, 6);    //generating the hour
+                    } while (newTT[hour, day] != null);
+                    newTT[hour, day] = aCourse.Name;    //adding the name of the course in the timetable
+                                                        //adding spaces
+                    while (newTT[hour, day].Length != 10)
+                    {
+                        newTT[hour, day] += " ";
                     }
-                } while (day == -1);
+                }
             }
-            for (int i = 0; i < newTT.Count(); i++)
+            //Writing on the console
+            for (int i = 0; i < newTT.GetLength(0); i++)
             {
-                for (int j = 0; j < newTT[i].Count(); j++)
+                for (int j = 0; j < newTT.GetLength(1); j++)
                 {
-                    Console.Write($" {newTT[i][j]} ");
+                    if (newTT[i, j] != null) Console.Write($"{newTT[i, j]}");
+                    else
+                    {
+                        Console.Write(new string(' ', newTT[0, j].Length)); //Filling blanks with spaces in order not to shift the timetable
+                    }
                 }
                 Console.WriteLine();
             }
         }
+        //function that shows every payment a student made
         public void TrackPayment(Student student)
         {
             int totalPayment = 0;
-            foreach (int item in student.Payment)
+            foreach (int item in student.Payment)   //display a list of every payment
             {
                 Console.WriteLine(item);
                 totalPayment += item;
             }
-            Console.WriteLine(totalPayment);
+            Console.WriteLine($"-----------------------------\nThe total amount is  : {totalPayment}"); //display the total amount spent
         }
-        public void AddStudent(List<Course> classes, int year, List<int> payment, List<List<int>> notes, List<bool> attendance)
+        //create a student 
+        public void AddStudent(List<Course> classes, int year, List<int> payment, List<List<int>> notes, List<bool> attendance, int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber)
         {
-            new Student(classes, year, payment, notes, attendance);
-            Console.WriteLine($"the student : {Name} has been added");
+            new Student(classes, year, payment, notes, attendance, ID, name, password, adress, inscriptionDate, phoneNumber);
+            Console.WriteLine($"the student : {Name} has been added");  //confirmation that the student has been created
         }
     }
 }
