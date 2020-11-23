b8ae481c5f4c9b48cf6a6e5fe1ee6372db4c36d9
nicolasgclves <71397510+nicolasgclves@users.noreply.github.com>
2020-11-23 15:00:20 +0100

    update réunion zoom

--- ConsoleApp6/ConsoleApp6/FacultyMember.cs
+++ ConsoleApp6/ConsoleApp6/FacultyMember.cs
@@ -7,52 +7,62 @@ using System.Threading.Tasks;
 namespace Project
 {
     // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
-    class FacultyMember : User
-    {
-        public List<Course> courses = new List<Course>();
-
-
-
-        public FacultyMember(List<Course> courses, int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber) : base(ID, name, password, adress, inscriptionDate, phoneNumber)
+    public class FacultyMember : User  //This class inherit user
         {
-            this.courses = courses;
-        }
+            public List<Course> courses = new List<Course>();
+            
 
 
-        public double GradeAStudent(Student student)
-        {
-            double grade = 0;
-            Console.WriteLine("Which grade would you assign to" + student);
-            grade = Convert.ToInt32(Console.ReadLine());
-            return grade;
-        }
+            public FacultyMember(List<Course> courses, int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber) : base(ID, name, password, adress, inscriptionDate, phoneNumber)
+            {
+                this.courses = courses;
+            }
 
-        public void AttendThere(Student student)
-        {
-            bool attendance = true;
-            student.Attendance.Add(attendance);
-        }
-        public void AttendAbsent(Student student)
-        {
-            bool attendance = false;
-            student.Attendance.Add(attendance);
-        }
 
-        public void CreateExam()
-        {
-            /// ? 
-        }
+            public void GradeAStudent(Student student) // method used to Grade a student and to save it in a text file
+            {
+                double grade = 0;
+                Console.WriteLine("Which grade would you assign to" + student);
+                grade = Convert.ToInt32(Console.ReadLine());
+                student.notes[0].Add(grade);         // writing the note in the Student Grade List    
+                Console.WriteLine("The grade " + grade + " Has been succesfully added to " + student); // Confirmation Sentence 
+                Console.ReadKey();
+            }
 
-        public void EditCoursePlan(Course course)
-        {
-            Console.WriteLine("There is the actual courseplan :");
-            Console.WriteLine(course.CoursePlan);
-            Console.WriteLine();
-            Console.WriteLine("Type the new courseplan : ");
-            course.CoursePlan = Console.ReadLine();
-            Console.WriteLine("There is the new courseplan");
-            Console.WriteLine(course.CoursePlan);
-            Console.ReadKey();
+            public void AttendThere(Student student)   // method used to Attend Present a student 
+            {
+                bool attendance = true;
+                student.Attendance.Add(attendance);    // writing in the Student attendance List that he is Present         
+                Console.WriteLine(student + " was well written present at your course ");  // confirmation sentence
+                Console.ReadKey();
+            }
+            public void AttendAbsent(Student student)   // method used to Attend Absent a student 
+            {
+                bool attendance = false;
+                student.Attendance.Add(attendance);  // writing in the Student attendance List that he is absent
+                Console.WriteLine(student + " was well written present at your course ");  // confirmation sentence
+                Console.ReadKey();
+            }
+
+            public void CreateExam(string name, double duration, FacultyMember prof, string room, List<Student> clas)  // method used to Create an Exam
+            {
+                Exam exam = new Exam(name, duration, prof, room, clas);  // here is the new Object Exam
+                Console.WriteLine("This Exam as been created with succes"); // confirmation sentence
+                Console.ReadKey();
+            }
+
+            public void EditCoursePlan(Course course)  // method used to Edit a Course Plan of a specific course
+            {
+                Console.WriteLine("There is the actual courseplan :");
+                Console.WriteLine(course.CoursePlan);
+                Console.WriteLine();
+                Console.WriteLine("Type the new courseplan : ");
+                course.CoursePlan = Console.ReadLine();
+                Console.WriteLine("There is the new courseplan");
+                Console.WriteLine(course.CoursePlan);
+                Console.ReadKey();
+            }
+
         }
 
     }
