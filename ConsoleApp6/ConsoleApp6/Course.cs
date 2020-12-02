109d9ba4479b5590d834316edab53fbfaa8bec2e
VictorKzo <72878097+VictorKzo@users.noreply.github.com>
2020-12-02 17:04:51 +0100

    Update Course.cs
    final course class

--- ConsoleApp6/ConsoleApp6/Course.cs
+++ ConsoleApp6/ConsoleApp6/Course.cs
@@ -1,31 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project
+namespace Main_projet
 {
-    // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
-    public class Course 
+    public class Course
 
     {
-        string name;
-        double duration;
-        string coursePlan;
-        string room;
-        private FacultyMember prof;
+        public string name;
+        public double duration;
+        public string coursePlan;
+        public FacultyMember prof;
+        public string room;
+        public List<Student> clas;
 
-        public Course(string name, double duration, string coursePlan, FacultyMember prof, string room)
+        public Course(string name, double duration, string coursePlan, FacultyMember prof, string room, List<Student> clas)
         {
             this.name = name;
             this.duration = duration;
             this.coursePlan = coursePlan;
             this.prof = prof;
             this.room = room;
+            this.clas = clas;
         }
 
-        public string Name => name;
+        public string Name { get; set; }
         public double Duration => duration;
         public string CoursePlan { get; set; }
         public string Room => room;
