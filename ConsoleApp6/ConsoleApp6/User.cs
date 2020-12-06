2c4f50efd983250f6a07bbecb802dedfd93cdb92
BenjaminTOUD <71397536+BenjaminTOUD@users.noreply.github.com>
2020-12-06 18:04:47 +0100

    Update User.cs

--- ConsoleApp6/ConsoleApp6/User.cs
+++ ConsoleApp6/ConsoleApp6/User.cs
@@ -1,29 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project
+namespace Main_projet
 {
-    // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
     public abstract class User //User is abstract - it can only be created as a Student, Tutor, Admin, or FacultyMember
     {                          //There fore it has no constructor.
-        //Attributes, things common to all Users - clear and simple attribute names
+                               //Attributes, things common to all Users - clear and simple attribute names
         #region Attributes                     
-        int ID;                 
-        string name;
-        private string password;
-        private string adress;
-        private string inscriptionDate;
-        private string phoneNumber;
-		#endregion
-		//Public Properties for User to work in tandem with its inherited classes
+        public int ID;
+        public string name;
+        public string password;
+        public string adress;
+        public string inscriptionDate;
+        public string phoneNumber;
+        #endregion
+        //Public Properties for User to work in tandem with its inherited classes
         //only get. Sets are handled through methods for some attributes only
-		#region Properties
-		public string Name
+        #region Properties
+        public string Name
         {
             get { return name; }
+
         }
 
         public string Adress
@@ -41,10 +41,19 @@ namespace Project
             get { return phoneNumber; }
         }
 
-		#endregion
+        public User(int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.password = password;
+            this.adress = adress;
+            this.inscriptionDate = inscriptionDate;
+            this.phoneNumber = phoneNumber;
+        }
+        #endregion
         //User-wide methods
-		#region Methods
-		public bool Login(string passInput) //Simple logging in attempt - will return false if the input is different to the registered password
+        #region Methods
+        public bool Login(string passInput) //Simple logging in attempt - will return false if the input is different to the registered password
         {
             return passInput == password;
         }
@@ -55,9 +64,10 @@ namespace Project
 
         public override string ToString() //User-Wide ToString method - Only outputs User available data, without phone number and adress
         {
-            return $"Student card : Name : {name} Student ID : {ID} Signed up the : {inscriptionDate}";
+            return $"User card : Name : {name} Student ID : {ID} Signed up the : {inscriptionDate}";
         }
 
-		#endregion
-	}
+        #endregion
+    }
+
 }
