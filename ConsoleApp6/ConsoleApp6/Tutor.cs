75f0dc8abf557b266fd94fc49e9bbe3f9c52c9f7
tioutbonnefont <71397371+tioutbonnefont@users.noreply.github.com>
2020-12-06 17:27:38 +0100

    Tutor final version
    All bug fixed

--- ConsoleApp6/ConsoleApp6/Tutor.cs
+++ ConsoleApp6/ConsoleApp6/Tutor.cs
@@ -4,20 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project
+namespace Main_projet
 {
-    // 23166 Victor CAZAUX, 23163 Théo UNDERWOOD, 23167 Nicolas GONCALVES, 23206 BENJAMIN TOUBIANA, 23178 FOUCAUD BONNEFONT, 23174 Thomas CULINO
-   
     
- class Tutor:FacultyMember         //This class inherit from FacultyMember, which is already inherited from user
+        public class Tutor : FacultyMember         //This class inherit from FacultyMember, which is already inherited from user
         {
-            List<Course> courseList; //Represent the list of courses that the Faculty member can teach
+            public List<Course> courseList; //Represent the list of courses that the Faculty member can teach
 
             public Tutor(List<Course> courseList, int ID, string name, string password, string adress, string inscriptionDate, string phoneNumber) : base(courseList, ID, name, password, adress, inscriptionDate, phoneNumber)     //Constructor
             {
                 this.courseList = courseList;
             }
-            
+
             public int[] IsStudent(Student student)   //Check if the student is a class of the tutor
             {
                 int[] result = new int[2];
@@ -27,16 +25,20 @@ namespace Project
                 {
                     for (int j = 0; j < courseList[i].clas.Count; j++)     //Going all the way through the students of the tutor
                     {
-                        if (courseList[i].clas[j] == student)             //If the student appears in a class
+                        if (courseList[i].clas[j].name == student.name)             //If the student appears in a class
                         {
                             result[0] = i;
                             result[1] = j;                   //We get the coordinate of him in the files
                             break;
                         }
                     }
-
                 }
                 return result;
+            }
+
+            public void CreateExam()
+            {
+
             }
 
             public void StudentDetail(Student student)           //To get personal details of a student
@@ -45,7 +47,7 @@ namespace Project
                 {
                     Console.WriteLine("This student is not in any of your courses");
                 }
-                
+
                 else
                 {
                     int i = IsStudent(student)[0];
@@ -54,4 +56,24 @@ namespace Project
                 }
 
             }
+
+
+
+            public void TeachACourse(string name, List<Course> courses)
+            {
+
+                if (Program.CheckCourse(name, courses) != -1)           //If the course exist
+                {
+                    this.courses.Add(courses[Program.CheckCourse(name, courses)]);            //The teacher is now teaching the course
+                    Console.WriteLine("You are now teaching " + courses[Program.CheckCourse(name, courses)].name + ".");
+                }
+                if (Program.CheckCourse(name, courses) == -1)          //Otherwise
+                {
+                    Console.WriteLine("This course doesn't exist");
+                }
+            }
+
         }
+        
+    
+}
